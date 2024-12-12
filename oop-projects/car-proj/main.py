a0b3dcb95ba8f82f9b41e4937a0c6c5168da1e39
vitaliyprotsiuk <xiz60606@gmail.com>
2024-12-13 00:03:59 +0200

    updated_folder

--- oop-projects/car-proj/main.py
+++ oop-projects/car-proj/main.py
@@ -2,9 +2,11 @@ from car import Car
 from car_filter import CarFilter
 from car_shop import CarShop
 
+
 nazar_dream_car = Car("Porsche Taycan", 300000, 2024, 280)
 vitalik_dream_car = Car("Tesla Plaid", 200000, 2022, 282)
 mattew_dream_car = Car("Toyota Land Cruiser 200", 100000, 2024, 180)
+
 
 it_club_dream_cars = [nazar_dream_car, vitalik_dream_car, mattew_dream_car]
 
