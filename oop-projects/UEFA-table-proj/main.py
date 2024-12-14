6f249f4e8e6c29ca98af57c3d38bb3a0f080ef00
vitaliyprotsiuk <xiz60606@gmail.com>
2024-12-15 01:13:51 +0200

    UEFA-proj update

--- oop-projects/UEFA-table-proj/main.py
+++ oop-projects/UEFA-table-proj/main.py
@@ -1,9 +1,9 @@
 from team import Team
 from table import Table
 
-liverpool = Team('Ліверпуль', 6, 6, 0, 0, 13, 1, '✅ ✅ ✅ ✅ ✅')
-barcelona = Team('Барселона', 6, 5, 0, 1, 21, 7, '✅ ✅ ✅ ✅ ✅')
-arsenal = Team('Арсенал  ', 6, 4, 1, 1, 12, 5, '✅ ❌ ✅ ✅ ✅')
+liverpool = Team('Ліверпуль', 6, 6, 0, 0, 13, 1, 18, '✅ ✅ ✅ ✅ ✅')
+barcelona = Team('Барселона', 6, 5, 0, 1, 21, 7, 15, '✅ ✅ ✅ ✅ ✅')
+arsenal = Team('Арсенал  ', 6, 4, 1, 1, 11, 2, 13, '✅ ❌ ✅ ✅ ✅')
 
 array_teams = [liverpool, barcelona, arsenal]
 
--- oop-projects/UEFA-table-proj/team.py
+++ oop-projects/UEFA-table-proj/team.py
@@ -10,7 +10,7 @@ class Team:
     _score = 0
     _last_five = None
 
-    def __init__(self, name, games, wins, loses, draws, goals, skipped_balls, last_five):
+    def __init__(self, name, games, wins, loses, draws, goals, skipped_balls, score, last_five):
         self._name = name
         self._games = games
         self._wins = wins
@@ -18,11 +18,12 @@ class Team:
         self._draws = draws
         self._goals = goals
         self._skiped_balls = skipped_balls
-        if self._balls_diference < 9:
+        self._balls_diference = goals - skipped_balls
+        if self._balls_diference < 10:
             self._balls_diference = str(self._balls_diference)
             self._balls_diference = f' {self._balls_diference}'
         else: self._balls_diference = goals - skipped_balls
-        self._score = wins * 3 + draws
+        self._score = score
         self._last_five = last_five
         
     def __str__(self):
